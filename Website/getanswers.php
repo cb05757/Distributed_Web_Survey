e784025047decba4595f4871ae78ca6c455994d3
cb05757 <chrisbeyer@gsu-141-165-115-176.vl112.dhcp.georgiasouthern.edu>
2014-12-01 13:34:44 -0500

    Improved Stats page
    Improved Stats page to work with multiple question types

---
+++ Website/getanswers.php
@@ -0,0 +1,102 @@
+<?php
+include_once 'includes/db-connect.php';	// mysqlinects to the DB. DB variable ->>> $mysqli
+$q = intval($_GET['q']);		// Gets 'q' variable passed,'q' is the ID 'question_id' sent when the user selects a question
+// Get all answers for the question 
+$sql="SELECT * FROM answer_tbl JOIN question_tbl On answer_tbl.answer_question=question_tbl.question_id WHERE answer_question =".$q." ";
+$sql = $mysqli->real_escape_string($sql);	// Helps prevent SQL inject by ignoring string literals
+$result = $mysqli->query($sql);
+if(! $result){
+	echo "error getting answers data";
+}
+$row_count = $result->num_rows;		// Gets the number of rows/objects/ answers for this question
+$answer = "";
+$stronglyAgree = 0;
+$agree = 0;
+$neither = 0;
+$disagree = 0;
+$stronglyDisagree = 0;
+$questionType = 0;
+// Find numbers on above variables
+while($row = mysqli_fetch_array($result)) {
+	$questionType = $row['question_type'];
+	// if question type = 1
+	$answer = "";
+	if($questionType == 1){
+		if($row['answer_value'] == 1){
+			$stronglyAgree++;
+		}else if($row['answer_value'] == 2){
+			$agree++;
+		}else if($row['answer_value'] == 3){
+			$neither++;
+		} else if($row['answer_value'] == 4){
+			$disagree++;
+		} else if($row['answer_value'] == 5){
+			$stronglyDisagree++;
+		}
+	}else if($questionType == 2){
+
+	}else if($questionType == 3){
+
+	}
+}
+// Get percentages 
+$stronglyAgree = ($stronglyAgree / $row_count ) * 100;
+$agree = ($agree / $row_count ) * 100;
+$neither = ($neither / $row_count ) * 100;
+$disagree = ($disagree / $row_count ) * 100;
+$stronglyDisagree = ($stronglyDisagree / $row_count ) * 100;
+echo "
+<h3> The Stats </h3>
+<table border='1'>
+<tr>
+<th># of times this question was answered</th>
+<th>The Answers</th>
+</tr>";
+// This Seeks back to the first Row so we can go through the object again.
+mysqli_data_seek($result,0);
+while($row2 = mysqli_fetch_array($result)) {
+	// Getting and setting the answer that the user gave
+	$answer = "";
+	if($questionType == 1){
+		if($row2['answer_value'] == 1){
+			$answer = "Strongly Agree";
+		}else if($row2['answer_value'] == 2){
+			$answer = "Agree";
+		}else if($row2['answer_value'] == 3){
+			$answer = "Neither";
+		} else if($row2['answer_value'] == 4){
+			$answer = "Disagree";
+		} else if($row2['answer_value'] == 5){
+			$answer = "Strongly Disagree";
+		}
+	} else if($questionType == 2){
+
+	} else if($questionType == 3){
+		$answer = $row2['answer_value'];
+	}
+  echo "<tr>";
+  echo "<td>" . $row_count. "</td>";		// outputs the number times this question has been answered
+  echo "<td>" . $answer . "</td>";			// outputs the different answers given
+  echo "</tr>";
+}
+echo "</table>";
+echo "<br> <br> <br>";
+ // Here is the table that shows the percentages of what the answers were 
+if($questionType == 1){
+	echo "<table border='1'>
+	<th> % Strongly Agrees </th>
+	<th> % Agrees </th>
+	<th> % Neither </th>
+	<th> % Disagree </th>
+	<th> % Strongly Disagrees <th>";
+	echo "<tr>";
+	echo "<td>" . $stronglyAgree . "</td>";
+	echo "<td>" . $agree . "</td>";
+	echo "<td>" . $neither . "</td>";
+	echo "<td>" . $disagree . "</td>";
+	echo "<td>" . $stronglyDisagree.  "</td>";
+	echo "</tr>";
+	echo "</table>";
+}
+mysqli_close($mysqli);
+?> 
